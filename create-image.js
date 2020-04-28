88688400fb9f02e6c501e9ce78d49b793547e628
vanminh1310 <taminh39@gmail.com>
2020-04-28 14:51:12 +0700

    first commit

--- create-image.js
+++ create-image.js
@@ -78,7 +78,7 @@ function CreateImage() {
 
         ctx.font = '20px arial,sans-serif-light,sans-serif';
         let heightText = wrapText(ctx, description, paddingLeft, 20, canvas.width - paddingRight, 30);
-        
+        const lerniLogo = await loadImage('lerni.dev.png');
         let cordFitImage = getCordFitImage(lerniLogo, heightText, MAX_WIDTH_CANVAS);
         ctx.drawImage(lerniLogo, cordFitImage.x, cordFitImage.y, lerniLogo.width * cordFitImage.scale, lerniLogo.height * cordFitImage.scale);
 
